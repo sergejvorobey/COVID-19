327c51d4aa834b9d89dd22487b83f35ee07b6901
sergeyvorobey <phanta2408@yandex.ru>
2020-04-03 20:16:02 +0300

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by Sergey Vorobey on 03/04/2020.
+//  Copyright © 2020 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
